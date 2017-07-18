9b4709be312fa2e0ed5a8ab6dde9f74b5e1e495f
Tim Parsons <tim.m.parsons@hotmail.com>
2017-07-17 17:58:29 -0700

    revised react

---
+++ src/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class NavBar extends React.Component{
+  render() {
+    const pages = ["Home", "About", "Menu", "Contact"];
+    const navLinks = pages.map(page => {
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      )
+    });
+
+    return <nav>{navLinks}</nav>
+  }  
+}
